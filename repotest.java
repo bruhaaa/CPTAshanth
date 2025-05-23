9c59c0e828acf066252ad20a2aff70c9ee4033e3
bruhaaa <ashanth.raveendran26@ycdsbk12.ca>
2025-05-23 10:51:33 -0400

    added more prints

--- repotest.java
+++ repotest.java
@@ -4,7 +4,8 @@ public class repotest{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("Hello World");
-		
+		con.println("TGIf");
+		con.println("bruh:");
 		
 	}
 }
