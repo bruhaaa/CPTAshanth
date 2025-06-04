bebeb7948f14ace909b26a3d676280ce56dc2c01
bruhaaa <ashanth.raveendran26@ycdsbk12.ca>
2025-06-04 10:48:20 -0400

    added stuff

--- blackjack.java
+++ blackjack.java
@@ -1,11 +1,34 @@
 import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 public class blackjack{
 	public static void main(String[] args){
-		Console con = new Console();
-	
-	
+		Console con = new Console("Blackjack", 1280, 720);
+		String strUser;
+		int intInput;
+		int intCard[][];
+		int intBal = 1000;
+		
+		con.println("                            Play (Enter 1)\n");
+		con.println("                        View Leaderboard (Enter 2)\n");
+		con.println("                              Quit (Enter 3)");
+		intInput = con.readInt();
+		
+		
+		if(intInput == 1){
+			// this takes them to game
+			
+		}else if(intInput == 2){
+			// views leaderboard
+			
+		}else if(intInput == 3){
+			//quits game
+			
+		}
+		
+		
+		
 	}
-
 
 }
