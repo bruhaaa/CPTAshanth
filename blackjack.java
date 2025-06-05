f63efd90c0457ad175bfdf7ecdcd4ad0b99f4581
bruhaaa <ashanth.raveendran26@ycdsbk12.ca>
2025-06-05 11:00:22 -0400

    Update blackjack.java

--- blackjack.java
+++ blackjack.java
@@ -10,7 +10,35 @@ public class blackjack{
 		int intCard[][];
 		int intBal = 1000;
 		
-		con.println("                            Play (Enter 1)\n");
+		// deck of cards
+		intCard = new int[52][3];
+		
+		//the diamonds
+		//diamond ace
+		intCard[0][0] = 1;
+		intCard[0][1] = 1;
+		intCard[0][2] = (int)(Math.random()*100+1);
+		
+		//2 of diamond
+		intCard[1][0] = 2;
+		intCard[1][1] = 1;
+		intCard[1][2] = (int)(Math.random()*100+1);
+		
+		// 3 of diamond
+		intCard[2][0] = 3;
+		intCard[2][1] = 1;
+		intCard[2][2] = (int)(Math.random()*100+1);
+		
+		//4 of diamond
+		
+	}
+
+}
+
+
+//Later
+
+		/*con.println("                            Play (Enter 1)\n");
 		con.println("                        View Leaderboard (Enter 2)\n");
 		con.println("                              Quit (Enter 3)");
 		intInput = con.readInt();
@@ -25,10 +53,4 @@ public class blackjack{
 		}else if(intInput == 3){
 			//quits game
 			
-		}
-		
-		
-		
-	}
-
-}
+		}*/
