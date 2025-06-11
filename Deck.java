dc4c31723c488507d0ad27945f57c5b409bba490
bruhaaa <ashanth.raveendran26@ycdsbk12.ca>
2025-06-11 18:45:01 -0400

    Finished the deck

--- Deck.java
+++ Deck.java
@@ -5,31 +5,44 @@ public class Deck{
 		Console con = new Console();
 		int intDeck[][];
 		intDeck = new int[52][3];
-		int intRandom = (int)(Math.random()*100+1);
 		int intSuit; 
 		int intValue;
 		
-		// theres 52 rows and 3 columns
-		
-		//column 0 is the value
-		//column 1 is the suit
-		//column 2 is the random number
-		// we should loop the rows to 52 and columns up to 3
-		// values up to 13
-		int intCount;
-		
-		
-			
-		for(intSuit = 1; intSuit <= 4; intSuit++){
-			for(intValue = 1; intValue <=13; intValue++){
-				intDeck[intValue][0] = intValue;
-				intDeck[intValue][1]= intSuit;
-				intDeck[intValue][2] = intRandom;		
-				
-			}
-			
-			
-		}
+		 // Initialize deck with random numbers
+        for(intSuit = 1; intSuit <= 4; intSuit++){
+            for(intValue = 1; intValue <= 13; intValue++){
+                int intRandom = (int)(Math.random() * 100 + 1);
+                intDeck[(intSuit - 1) * 13 + (intValue - 1)][0] = intValue;
+                intDeck[(intSuit - 1) * 13 + (intValue - 1)][1] = intSuit;
+                intDeck[(intSuit - 1) * 13 + (intValue - 1)][2] = intRandom;        
+            }
+        }
+
+
+        // Bubble sorts the deck, shuffling everything
+        int intCount; 
+        int intCount2;
+        int intTempSuit ;
+        int intTempValue; 
+        int intTempRand;
+
+        for(intCount = 0; intCount < 51; intCount++){
+            for(intCount2 = 0; intCount2 < 51 - intCount; intCount2++){
+                if(intDeck[intCount2][2] > intDeck[intCount2 + 1][2]){
+                    intTempValue = intDeck[intCount2][0];
+                    intTempSuit = intDeck[intCount2][1];
+                    intTempRand = intDeck[intCount2][2];
+
+                    intDeck[intCount2][0] = intDeck[intCount2 + 1][0];
+                    intDeck[intCount2][1] = intDeck[intCount2 + 1][1];
+                    intDeck[intCount2][2] = intDeck[intCount2 + 1][2];
+
+                    intDeck[intCount2 + 1][0] = intTempValue;
+                    intDeck[intCount2 + 1][1] = intTempSuit;
+                    intDeck[intCount2 + 1][2] = intTempRand;
+                }
+            }
+        }
 		
 	
 	
